715fce6cdd8396b52f604b5e651abe15f7fb6e33
CodingMa <897566085@qq.com>
2018-11-06 17:01:56 +0800

    update Ch03

--- Ch03/dataStructure.py
+++ Ch03/dataStructure.py
@@ -33,12 +33,57 @@ def object2():
     gen = range(0, 10, 2)
     print(list(gen))
 
+def object3():
+    a = [2, 4, 6, 8, 10, 'Jack']
+    a.append(12)
+    a.insert(1, 10)    # [2, 10, 4, 6, 8, 10, 12]
+    a.pop(3)    # [2, 10, 4, 8, 10, 12]
+    print(a)
+    print('Jack' in a)  # True
+
+def object4():
+    a = [4, None, 'foo'] + [7, 8, (2, 3)]
+    print(a)
+    x = [4, None, 'foo']
+    x.extend([7, 8, (2, 3)])    # extend比+效率高
+    print(x)
+
+import bisect
+
+def object5():
+    a = [9, 5, 1, 6, 8, 19]
+    a.sort()
+    print(a)
+
+def object6():
+    seq = [7, 2, 3, 7, 5, 6, 0, 1]
+    print(seq[1:3])
+    seq[3:4] = [6, 3]   # [7, 2, 3, 6, 3, 5, 6, 0, 1]
+    print(seq)
+    print(seq[:3])  # [7, 2, 3]
+    print(seq[3:])  # [6, 3, 5, 6, 0, 1]
+    print(seq[-3:]) # [6, 0, 1]
+
+def object7():
+    a = {'a' : 'some value', 'b' : [1, 2, 3, 4]}
+    a['c'] = 'Jack'    # {'a': 'some value', 'b': [1, 2, 3, 4], 'c': 'Jack'}
+    #a.pop('a')  # {'b': [1, 2, 3, 4], 'c': 'Jack'}
+    print(a)
+    b = list(a.keys())
+    print(b)
+    c = list(a.values())
+    print(c)
 
 
 def test():
     print("-------------start------------------")
     #object1()
-    object2()
+    #object2()
+    #object3()
+    #object4()
+    #object5()
+    #object6()
+    object7()
     print("-------------end------------------")
 
 if __name__ == '__main__':
