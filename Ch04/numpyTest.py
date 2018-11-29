fe79743f43ed29dae90fee3f41bb925deb0b485d
youkee <2294764468@qq.com>
2018-11-29 17:22:45 +0800

    update ch04

---
+++ Ch04/numpyTest.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def object():
+    print()
+
+def test():
+    print("-------------start------------------")
+    print("-------------end------------------")
+
+if __name__ == '__main__':
+    test()
